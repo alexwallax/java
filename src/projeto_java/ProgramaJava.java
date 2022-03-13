a7ad6c11b8f4d8e5e5c3d20b546204122b7d7a8d
Alex Wallace <alexwallax@gmail.com>
2022-03-12 21:35:22 -0300

    commit teste

--- src/projeto_java/ProgramaJava.java
+++ src/projeto_java/ProgramaJava.java
@@ -1,9 +1,11 @@
 package projeto_java;
 
 public class ProgramaJava {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Programa Java");
+		System.out.println("Teste");
+
 	}
 
 }
